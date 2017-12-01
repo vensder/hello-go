0bcdc84ea38da0aada5db5270445b0707421fcb3
Dmitrii Makarov <dmitrii_makarov@epam.com>
2017-12-01 19:48:35 +0300

    removed get pub finc

--- webserver.go
+++ webserver.go
@@ -40,29 +40,6 @@ func getMetaData(meta_data_item string) string {
 	return "Status is Not OK for " + meta_data_item
 }
 
-func gePublicIP() string {
-	timeout := time.Duration(1 * time.Second)
-	client := http.Client{
-		Timeout: timeout,
-	}
-	resp, err := client.Get("http://169.254.169.254/latest/meta-data/public-ipv4")
-	if err != nil {
-		fmt.Println("Can't get public IP via aws meta data")
-		return "127.0.0.1"
-	}
-	defer resp.Body.Close() // Close body only if response non-nil
-	if resp.StatusCode == http.StatusOK {
-		bodyBytes, err2 := ioutil.ReadAll(resp.Body)
-		if err2 != nil {
-			fmt.Println("Can't get responce Body")
-			return "127.0.0.1"
-		}
-		bodyString := string(bodyBytes)
-		return bodyString
-	}
-	return "Status is Not OK"
-}
-
 func main() {
 	meta_data_items := []string{
 		"ami-id",
@@ -82,15 +59,12 @@ func main() {
 	fmt.Println("Server starting...")
 	hostname := hostname()
 	fmt.Println("Hostname: " + hostname)
-	gePublicIP := gePublicIP()
-	fmt.Println("IP Addr: " + gePublicIP)
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, ("<p>URL Path: " + r.URL.Path + "</p>"))
 		fmt.Fprintf(w, ("<h1>Host: " + hostname + "</h1>"))
-		fmt.Fprintf(w, ("<h1>IP Addr: " + gePublicIP + "</h1>"))
 		for _, item := range meta_data_items {
-			fmt.Fprintf(w, ("<h1>" + item + ": " + meta_data_items_map[item] + "</h1>"))
+			fmt.Fprintf(w, ("<h2>" + item + ": " + meta_data_items_map[item] + "</h2>"))
 		}
 		fmt.Println(hostname)
 	})
